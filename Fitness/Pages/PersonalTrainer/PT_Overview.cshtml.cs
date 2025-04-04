529cb9e2a6d5a2103030c2e317f8d263b1be0f7d
Sjors Beerens <546732@student.fontys.nl>
2025-04-04 11:00:47 +0200

    Add PT_OverviewModel Razor Page model class
    Introduced a new Razor Page model class `PT_OverviewModel` in the `Fitness.Pages.PersonalTrainer` namespace. This class inherits from `PageModel` and includes an empty `OnGet` method to handle GET requests. Added necessary `using` directives for `Microsoft.AspNetCore.Mvc` and `Microsoft.AspNetCore.Mvc.RazorPages`.

---
+++ Fitness/Pages/PersonalTrainer/PT_Overview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fitness.Pages.PersonalTrainer
+{
+    public class PT_OverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
