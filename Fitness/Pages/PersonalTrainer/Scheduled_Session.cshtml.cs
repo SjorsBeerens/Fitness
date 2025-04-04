3d94e28bde3a43d5fca69c93bafcf7966934618c
Sjors Beerens <546732@student.fontys.nl>
2025-04-04 11:02:50 +0200

    Add new Razor Pages for fitness app: Booking, Session, Profile
    Introduced three new Razor Pages: ConfirmBooking, Scheduled_Session, and TrainerProfile. Each page includes its corresponding .cshtml and .cshtml.cs files. Added @page and @model directives in the .cshtml files. Created new PageModel classes with empty OnGet methods for each page.

---
+++ Fitness/Pages/PersonalTrainer/Scheduled_Session.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fitness.Pages.PersonalTrainer
+{
+    public class Scheduled_SessionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Fitness/Pages/PersonalTrainer/TrainerProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fitness.Pages.PersonalTrainer
+{
+    public class TrainerProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
