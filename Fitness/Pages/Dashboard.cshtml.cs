cbbc00837594d6d1ff5be95716689c1a774dfc87
Sjors Beerens <546732@student.fontys.nl>
2025-04-25 11:20:13 +0200

    Update OnGet method in DashboardModel for session handling
    Modified the OnGet method to return IActionResult, enabling redirects. Added session check for "UserEmail" to redirect to the login page if the session does not exist, or display the dashboard if it does.

--- Fitness/Pages/Dashboard.cshtml.cs
+++ Fitness/Pages/Dashboard.cshtml.cs
@@ -5,8 +5,17 @@ namespace Fitness.Pages
 {
     public class DashboardModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            // Controleer of de sessie bestaat
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserEmail")))
+            {
+                // Als de sessie niet bestaat, redirect naar de loginpagina
+                return RedirectToPage("/Login/login");
+            }
+
+            // Als de sessie bestaat, toon de dashboardpagina
+            return Page();
         }
     }
 }
