3efb1ae15d966e91c40390176ce5e6a0dad58432
Sjors Beerens <546732@student.fontys.nl>
2025-05-09 10:41:11 +0200

    Implement session checks in page models
    Updated multiple page models to return IActionResult instead of void.
    Added user session validation to redirect to the login page if the session is not valid.
    Included necessary using directives in relevant files.

--- Fitness/Pages/Dashboard.cshtml.cs
+++ Fitness/Pages/Dashboard.cshtml.cs
@@ -7,14 +7,10 @@ namespace Fitness.Pages
     {
         public IActionResult OnGet()
         {
-            // Controleer of de sessie bestaat
             if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserEmail")))
             {
-                // Als de sessie niet bestaat, redirect naar de loginpagina
                 return RedirectToPage("/Login/login");
             }
-
-            // Als de sessie bestaat, toon de dashboardpagina
             return Page();
         }
     }
